7647f2c9f2c1cd30f5f1a3f6a07b40b5c87af736
byjose007 <byjose007@gmail.com>
2021-05-06 14:00:42 -0500

    Create user controller

---
+++ backend/src/apiServices/users/controllers/create-user.controller.js
@@ -0,0 +1,18 @@
+import CreateUserService from '../services/create-user.service';
+const createUserService = new CreateUserService();
+
+
+exports.saveUser = async (req, res) => {
+
+    const userDTO = req.body;
+    try {
+        const savedUser = await createUserService.createUser(userDTO);
+        res.status(201).json(savedUser);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: err,
+        });
+    }
+};
+
