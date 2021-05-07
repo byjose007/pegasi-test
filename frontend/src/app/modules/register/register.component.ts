82b5886697f26a0bb4f15aa7312f984987261c59
byjose007 <byjose007@gmail.com>
2021-05-06 23:39:57 -0500

    Create register component and FormModule

--- frontend/src/app/modules/register/register.component.ts
+++ frontend/src/app/modules/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', Validators.required],
+      sex: ['', Validators.required],
+      pregnancyOrBreastfeeding: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      telephone: ['', Validators.required],
+    });
+    // this.registerForm.valueChanges.subscribe(console.log);
+  }
+
+  submitForm() {
+    if (!this.registerForm.valid) {
+      return;
+    }
+    console.log(this.registerForm.value);
+    this.registerForm.reset();
+  }
 }
--- frontend/src/app/modules/register/register.module.ts
+++ frontend/src/app/modules/register/register.module.ts
@@ -1,9 +1,15 @@
+import { SharedModule } from './../../shared/shared.module';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
-
+// Material angular
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +17,13 @@ import { RegisterComponent } from './register.component';
   ],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    RegisterRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ]
 })
 export class RegisterModule { }
